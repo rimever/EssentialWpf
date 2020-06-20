173ddcd9f833db3fee5f5b498192355f8e367b75
rimever <mot_siva_ordin@hotmail.com>
2020-06-20 22:32:06 +0900

    P.77まで進めた。

---
+++ Chapter2_11/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter2_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMethod(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window {Title = "Show"};
+            window.Show();
+        }
+
+        private void UserVisibilityProperty(object sender, RoutedEventArgs e)
+        {
+            var window = new Window {Title = "Visibility = Visible"};
+            window.Visibility = Visibility.Visible;
+        }
+
+        private void ShowDialogMethod(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title = "ShowDialog",
+                Owner = this,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                ShowInTaskbar = false
+            };
+            window.ShowDialog();
+        }
+    }
+}
---
+++ Chapter2_13/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter2_13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static int _createCount;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = "ウインドウ" + _createCount;
+            _createCount++;
+        }
+
+        private void NewWindowClicked(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+
+        private void ListOpenWindows(object sender, RoutedEventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (Window window in Application.Current.Windows)
+            {
+                stringBuilder.AppendLine(window.Title);
+            }
+
+            MessageBox.Show(stringBuilder.ToString(), "Open Windows");
+        }
+    }
+}
