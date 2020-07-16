cd05f5352a939824d405fa979d8ba79f8a8912d0
rimever <mot_siva_ordin@hotmail.com>
2020-07-16 22:50:20 +0900

    6-9まで進めた。

---
+++ Chapter6_12/LocalNameTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+
+namespace Chapter6_12
+{
+    public class LocalNameTemplateSelector:DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is XmlElement data)
+            {
+                return (container as FrameworkElement)?.FindResource(data.LocalName) as DataTemplate;
+            }
+            return null;
+        }
+    }
+}
---
+++ Chapter6_9/Name.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Chapter6_9
+{
+    class Name : INotifyPropertyChanged
+    {
+        private string _first;
+        private string _last;
+
+        public string First
+        {
+            get => _first;
+            set
+            {
+                _first = value;
+                NotifyChanged("First");
+            }
+        }
+
+        public string Last
+        {
+            get => _last;
+            set
+            {
+                _last = value;
+                NotifyChanged("Last");
+            }
+        }
+
+        private void NotifyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
